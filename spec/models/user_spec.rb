bbc5fb380e000663731dd242ebe12a1d79ff572a
naotomineta <71373810+naotomineta@users.noreply.github.com>
2020-10-12 09:42:21 +0900

    2回目の修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,11 @@ RSpec.describe User, type: :model do
       another_user.valid?
       expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
+    it 'emailに@を含まない場合は登録できない' do
+      @user.email = "abc123.com.jp"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
+    end
     it 'passwordが空だと登録できない' do
       @user.password = ''
       @user.valid?
