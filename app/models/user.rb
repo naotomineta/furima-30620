afeecca145a66bafadcdc8305f08adb02b78aff8
naotomineta <71373810+naotomineta@users.noreply.github.com>
2020-10-12 11:12:25 +0900

    複数の正規表現の変数置き換え

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  zenkaku = /\A[ぁ-んァ-ン一-龥]+\z/
+  kananomi = /\A[ァ-ヶー－]+\z/
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name, format: { with: zenkaku}
+    validates :last_name, format: { with: zenkaku}
+    validates :first_name_kana, format: { with: kananomi }
+    validates :last_name_kana, format: { with: kananomi }
     validates :birth_date
   end
 
