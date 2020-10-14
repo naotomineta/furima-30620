556b75b56fd6bed18735bd4ae45603dc8d37310c
naotomineta <71373810+naotomineta@users.noreply.github.com>
2020-10-14 22:27:45 +0900

    商品の出品と保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,25 @@
 class ItemsController < ApplicationController
   def index
-    @items
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :info, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :schedule_delivery_id,:category_id, :image).merge(user_id: current_user.id)
   end
 end
